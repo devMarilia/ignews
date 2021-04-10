ae52a398e67cedfb043c5c80ae5d72e62079acee
devMarilia <devMarilia.frontend@gmail.com>
2021-04-10 17:11:08 -0300

    :up: atualização

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,8 @@ export default class MyDocument extends Document{
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,900;1,400&display=swap" rel="stylesheet"/>
+            
+                   <link rel="shortcut icon" href="favicon.png" type="image/png"/>
                 </Head>
                 <body>
                     <Main/>
