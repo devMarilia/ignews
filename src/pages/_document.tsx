4da8021bd50e5b49d3d1ab0dc890058086f31b2a
devMarilia <devMarilia.frontend@gmail.com>
2021-04-10 10:24:19 -0300

    :package: nova funcionalidade

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,6 @@ export default class MyDocument extends Document{
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,900;1,400&display=swap" rel="stylesheet"/>
-                    <title>ig.news</title>
                 </Head>
                 <body>
                     <Main/>
