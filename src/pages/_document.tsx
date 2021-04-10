2486116a6154abe3f6d93d849076f0a2aefb3e43
devMarilia <devMarilia.frontend@gmail.com>
2021-04-10 09:01:31 -0300

    :package: nova funcionalidade

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, {Html, Head, Main, NextScript} from 'next/document'
+export default class MyDocument extends Document{
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,900;1,400&display=swap" rel="stylesheet"/>
+                    <title>ig.news</title>
+                </Head>
+                <body>
+                    <Main/>
+                    <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+}
