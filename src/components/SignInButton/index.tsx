2507e79858bded7fff17fa2975c977f871bc8fa1
devMarilia <devMarilia.frontend@gmail.com>
2021-04-11 16:17:40 -0300

    :package: nova funcionalidade

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,27 @@
 import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 export default function SignInButton() {
-    const isUserLoggedIn = true;
-    return isUserLoggedIn ? (
+    const [session] = useSession()
+
+
+    return session ? (
         <button
         type="button"
         className={styles.signInButton}
+        onClick={() => signOut()}
         >
             <FaGithub color="#04d361"/>
-            devMarilia
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : (
         <button
         type="button"
         className={styles.signInButton}
+        onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417"/>
             Sign in with GitHub
