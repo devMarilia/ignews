b949bf3437f5a0356959cd458a74006368c1d42e
devMarilia <devMarilia.frontend@gmail.com>
2021-04-11 22:02:08 -0300

    :package: nova funcionalidade

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,55 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import NextAuth from "next-auth";
+import { signIn } from "next-auth/client";
+import Providers from "next-auth/providers";
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
 
 export default NextAuth({
- 
-  providers: [
-    Providers.GitHub({
+providers: [
+Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope:'read:user'
+      scope: 'read:user',
     }),
   ],
+
+  // jwt: {
+  //   signingKey: process.env.SIGNIN_KEY,
+  // },
+
+  callbacks: {
+    async signIn(user, account, profile) {
+    const { email } = user
+      
+    try {
+      await fauna.query( 
+        q.If(
+          q.Not(
+            q.Exists(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          ),
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          ),
+          q.Get(
+            q.Match(
+              q.Index('user_by_email'),
+              q.Casefold(user.email)
+            )
+          )
+        )
+      )
+
+      return true
+    } catch {
+      return false
+    }
+      
+    },
+  }
 })
